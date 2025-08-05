2de386131f3cd6da3121252d2e3291eeedb0e297
hawkess <getseriousjunk@gmail.com>
2025-08-05 03:07:37 -0600

    added vial support for bridge75

---
+++ keyboards/shortcut/bridge75/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+include keyboards/linker/wireless/wireless.mk
+WS2812_DRIVER_REQUIRED = yes
+WS2812_DRIVER = bitbang
+RGB_MATRIX_DRIVER = ws2812
+VIALRGB_ENABLE = yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
