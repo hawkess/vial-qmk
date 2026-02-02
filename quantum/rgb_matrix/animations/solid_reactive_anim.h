15468b2e6d598da5d565118ba13f1b674904f574
hawkess <50639790+hawkess@users.noreply.github.com>
2026-02-02 14:03:01 -0700

    revert solid_reactive_anim.h

--- quantum/rgb_matrix/animations/solid_reactive_anim.h
+++ quantum/rgb_matrix/animations/solid_reactive_anim.h
@@ -8,7 +8,7 @@ static hsv_t SOLID_REACTIVE_math(hsv_t hsv, uint16_t offset) {
 #            ifdef RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
     hsv.h = scale16by8(g_rgb_timer, qadd8(rgb_matrix_config.speed, 8) >> 4);
 #            endif
-    hsv.h += scale8(255 - offset, 140);
+    hsv.h += scale8(255 - offset, 64);
     return hsv;
 }
 
